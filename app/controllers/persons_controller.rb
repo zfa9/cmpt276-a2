c29772613c2d0f7e6427cad484ed4f7246090458
zfa9 <zfa9@sfu.ca>
2016-02-04 10:11:01 -0800

    Added the show page

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,9 +1,21 @@
 class PersonsController < ApplicationController
+  def show
+    @person = Person.find(params[:id])
+  end
+  
   def new
   end
 
   def create
-    
+    @person = Person.new(person_params)
+
+    @person.save
+    redirect_to @person
   end
+
+  private
+    def person_params
+      params.require(:person).permit(:name, :weight, :height, :color, :age, :sex)
+    end
 
 end
